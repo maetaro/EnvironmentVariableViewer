eadd3897111a22f78cd26d72ea1dcefb90650345
maetaro0614 <maetaro0614@gmail.com>
2015-09-19 11:38:32 +0900

    Tool of Viewing Windows Environment Valiable

---
+++ EnvironmentValiableViewer/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.colKey = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(642, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colKey,
+            this.colValue});
+            this.listView1.Location = new System.Drawing.Point(12, 62);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(705, 333);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // colKey
+            // 
+            this.colKey.Text = "Key";
+            this.colKey.Width = 250;
+            // 
+            // colValue
+            // 
+            this.colValue.Text = "Value";
+            this.colValue.Width = 600;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 18);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Process";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(76, 18);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "User";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(129, 18);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(65, 16);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Machine";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(248, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(426, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Value";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(278, 29);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(142, 19);
+            this.txtKey.TabIndex = 2;
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(466, 29);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(158, 19);
+            this.txtValue.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(219, 44);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "EnvironmentVariableTarget";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 407);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "EnvironmentValiableViewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.ColumnHeader colKey;
+        private System.Windows.Forms.ColumnHeader colValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ EnvironmentValiableViewer/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Search();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Search();
+        }
+
+        private void Search()
+        {
+            EnvironmentVariableTarget target = EnvironmentVariableTarget.Machine;
+            if (radioButton1.Checked)
+            {
+                target = EnvironmentVariableTarget.Process;
+            }
+            else if (radioButton2.Checked)
+            {
+                target = EnvironmentVariableTarget.User;
+            }
+            else if (radioButton3.Checked)
+            {
+                target = EnvironmentVariableTarget.Machine;
+            }
+            listView1.Items.Clear();
+            foreach (System.Collections.DictionaryEntry item in System.Environment.GetEnvironmentVariables(target))
+            {
+                if (String.IsNullOrWhiteSpace(txtKey.Text)
+                    && !item.Key.ToString().ToUpper().Contains(txtKey.Text.Trim().ToUpper()))
+                {
+                    continue;
+                }
+                if (String.IsNullOrWhiteSpace(txtValue.Text)
+                    && !item.Value.ToString().ToUpper().Contains(txtValue.Text.Trim().ToUpper()))
+                {
+                    continue;
+                }
+
+                ListViewItem subitem = new ListViewItem();
+                subitem.Text = item.Key.ToString();
+                subitem.SubItems.Add(item.Value.ToString());
+                listView1.Items.Add(subitem);
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            Form f = new Form();
+            ListView lv = new ListView();
+            lv.Columns.Add("Value", 500);
+            foreach (string item in listView1.SelectedItems[0].SubItems[1].Text.Split(';'))
+            {
+                lv.Items.Add(item);
+            }
+            lv.DoubleClick += (sender2, e2) =>
+                {
+                    ListView lv2 = (ListView)sender2;
+                    string path = lv2.SelectedItems[0].Text;
+                    if (!System.IO.Directory.Exists(path))
+                    {
+                        return;
+                    }
+                    System.Diagnostics.Process.Start("explorer.exe", path);
+                };
+            lv.View = View.Details;
+            lv.Dock = DockStyle.Fill;
+            f.Controls.Add(lv);
+            f.Text = listView1.SelectedItems[0].Text;
+            f.ShowDialog();
+        }
+
+    }
+}
