a3f0ba6ec0b4c459c0478c510a297dad8f7a594f
maetaro0614 <maetaro0614@gmail.com>
2015-09-19 14:51:30 +0900

    little bit change

--- EnvironmentValiableViewer/Form1.cs
+++ EnvironmentValiableViewer/Form1.cs
@@ -29,6 +29,8 @@ namespace WindowsFormsApplication1
 
         private void Search()
         {
+            listView1.Items.Clear();
+
             EnvironmentVariableTarget target = EnvironmentVariableTarget.Machine;
             if (radioButton1.Checked)
             {
@@ -42,25 +44,30 @@ namespace WindowsFormsApplication1
             {
                 target = EnvironmentVariableTarget.Machine;
             }
-            listView1.Items.Clear();
-            foreach (System.Collections.DictionaryEntry item in System.Environment.GetEnvironmentVariables(target))
-            {
-                if (String.IsNullOrWhiteSpace(txtKey.Text)
-                    && !item.Key.ToString().ToUpper().Contains(txtKey.Text.Trim().ToUpper()))
+            System.Environment.GetEnvironmentVariables(target)
+                .OfType<System.Collections.DictionaryEntry>()
+                .Where(item =>
                 {
-                    continue;
-                }
-                if (String.IsNullOrWhiteSpace(txtValue.Text)
-                    && !item.Value.ToString().ToUpper().Contains(txtValue.Text.Trim().ToUpper()))
+                    if (!string.IsNullOrWhiteSpace(txtKey.Text)
+                       && !item.Key.ToString().ToUpper().Contains(txtKey.Text.Trim().ToUpper()))
+                    {
+                        return false;
+                    }
+                    if (!string.IsNullOrWhiteSpace(txtValue.Text)
+                        && !item.Value.ToString().ToUpper().Contains(txtValue.Text.Trim().ToUpper()))
+                    {
+                        return false;
+                    }
+                    return true;
+                })
+                .ToList()
+                .ForEach(item =>
                 {
-                    continue;
-                }
-
-                ListViewItem subitem = new ListViewItem();
-                subitem.Text = item.Key.ToString();
-                subitem.SubItems.Add(item.Value.ToString());
-                listView1.Items.Add(subitem);
-            }
+                    ListViewItem subitem = new ListViewItem();
+                    subitem.Text = item.Key.ToString();
+                    subitem.SubItems.Add(item.Value.ToString());
+                    listView1.Items.Add(subitem);
+                });
         }
 
         private void listView1_DoubleClick(object sender, EventArgs e)
@@ -68,10 +75,10 @@ namespace WindowsFormsApplication1
             Form f = new Form();
             ListView lv = new ListView();
             lv.Columns.Add("Value", 500);
-            foreach (string item in listView1.SelectedItems[0].SubItems[1].Text.Split(';'))
-            {
-                lv.Items.Add(item);
-            }
+            var items = listView1.SelectedItems[0].SubItems[1].Text.Split(';')
+                .Select(item => new ListViewItem() { Text = item })
+                .ToArray();
+            lv.Items.AddRange(items);
             lv.DoubleClick += (sender2, e2) =>
                 {
                     ListView lv2 = (ListView)sender2;
